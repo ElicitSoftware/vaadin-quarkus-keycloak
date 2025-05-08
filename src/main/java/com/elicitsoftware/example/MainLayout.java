91974477a5b73cc1fd6821f7f573920de29f5158
Matthew Demerath <m.demerath@elicitsoftware.com>
2025-05-08 13:38:33 -0400

    Added logout button

--- src/main/java/com/elicitsoftware/example/MainLayout.java
+++ src/main/java/com/elicitsoftware/example/MainLayout.java
@@ -1,19 +1,27 @@
 package com.elicitsoftware.example;
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.Scroller;
 import com.vaadin.flow.component.sidenav.SideNav;
 import com.vaadin.flow.component.sidenav.SideNavItem;
-import com.vaadin.flow.server.VaadinSession;
 import com.vaadin.flow.theme.lumo.LumoUtility;
+import io.quarkus.oidc.OidcSession;
 import jakarta.annotation.PostConstruct;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.jwt.JsonWebToken;
 
 public class MainLayout extends AppLayout {
 
-    VaadinSession session = VaadinSession.getCurrent();
+    @Inject
+    OidcSession oidcSession;
+
+    @Inject
+    JsonWebToken accessToken;
 
     public MainLayout() {
     }
@@ -37,22 +45,29 @@ public class MainLayout extends AppLayout {
         Scroller navScroller = new Scroller(nav);
         navScroller.setClassName(LumoUtility.Padding.SMALL);
         addToDrawer(navScroller);
+        addToDrawer(new Button("logout"));
     }
 
     private SideNav getsideNav() {
         SideNav sideNav = new SideNav();
-
         sideNav.addItem(
-                new SideNavItem("Home -- @AnonymousAllowed", "/",
+                new SideNavItem("Home @AnonymousAllowed", "/",
                         VaadinIcon.INFO.create()),
-                new SideNavItem("User -- @RolesAllowed(\"user\")", "/basic", VaadinIcon.USER.create()),
-                new SideNavItem("Admin @RolesAllowed(\"admin\")", "/admin", VaadinIcon.LOCK.create()),
-                // TODO remove respondent form the session.
-                //Change this to login if not logged in
-                new SideNavItem("Logout", "",
-                        VaadinIcon.UNLINK.create())
+                new SideNavItem("User @RolesAllowed(\"user\")", "/basic", VaadinIcon.USER.create()),
+                new SideNavItem("Admin @RolesAllowed(\"admin\")", "/admin", VaadinIcon.LOCK.create())
+
         );
         return sideNav;
+    }
+    private Button getLogingButton(){
+
+        if (accessToken.getName() != null) {
+            return new Button("Logout", VaadinIcon.SIGN_OUT.create(), event -> {
+                oidcSession.logout();
+            });
+        } else {
+            return new Button("Login", VaadinIcon.SIGN_IN.create());
+        }
     }
 }
 
