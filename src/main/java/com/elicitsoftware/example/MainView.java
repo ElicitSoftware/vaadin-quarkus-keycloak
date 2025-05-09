64c891f4cc9368bbffabd03cac1b91143fe94fc8
Matthew Demerath <m.demerath@elicitsoftware.com>
2025-05-09 09:31:50 -0400

    Moved SecurityIdentity identity into the constructor

--- src/main/java/com/elicitsoftware/example/MainView.java
+++ src/main/java/com/elicitsoftware/example/MainView.java
@@ -1,10 +1,5 @@
 package com.elicitsoftware.example;
 
-import com.vaadin.flow.server.auth.AnonymousAllowed;
-import io.quarkus.security.identity.SecurityIdentity;
-import jakarta.annotation.security.PermitAll;
-import jakarta.inject.Inject;
-
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
@@ -12,6 +7,9 @@ import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import io.quarkus.security.identity.SecurityIdentity;
+import jakarta.annotation.security.PermitAll;
+import jakarta.inject.Inject;
 
 /**
  * The main view contains a button and a click listener.
@@ -23,30 +21,21 @@ public class MainView extends VerticalLayout {
     @Inject
     GreetService greetService;
 
-    @Inject
-    SecurityIdentity identity;
-
-    public MainView() {
-
-        if (identity != null && !identity.isAnonymous()) {
-            System.out.println("Authenticated user: " + identity.getPrincipal().getName());
-            System.out.println("Roles: " + identity.getRoles());
-        } else {
-            System.out.println("User is not authenticated.");
-        }
-
+    public MainView(SecurityIdentity identity) {
+        TextField textField = new TextField("Your name");
         if (identity != null && !identity.isAnonymous()) {
             if (identity.getRoles().contains("admin")) {
-                add(new Paragraph("You are also an admin!"));
-            } else if (identity.getRoles().contains("user")) {
-                add(new Paragraph("You are also a user!"));
+                add(new Paragraph(identity.getPrincipal().getName() + " you are an admin!"));
             }
+            if (identity.getRoles().contains("user")) {
+                add(new Paragraph(identity.getPrincipal().getName() + " you are also a user!"));
+            }
+            textField.setValue(identity.getPrincipal().getName());
         } else {
             add(new Paragraph("You are not authenticated."));
         }
 
         // Use TextField for standard text input
-        TextField textField = new TextField("Your name");
         textField.addThemeName("bordered");
 
         // Button click listeners can be defined as lambda expressions
