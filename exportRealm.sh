9319aa9b703e4a920b861c60eea3250cd08a9714
Matthew Demerath <mdemerat@med.umich.edu>
2025-05-16 08:29:53 -0400

    Working script to export a realm for this example.

--- exportRealm.sh
+++ exportRealm.sh
@@ -1,1 +1,3 @@
-docker exec -it bold_meitner sh -c "/opt/keycloak/bin/kc.sh export --log-level info --file /opt/keycloak/data/import/quarkus-realm-export.json --realm quarkus --users same_file"
+docker exec -it $1 sh -c "cp -rp /h2 /tmp; /opt/keycloak/bin/kc.sh export --log-level info --file /tmp/vaadin-realm-export.json --realm vaadin --users same_file --optimized --db-url 'jdbc:h2:file:/tmp/h2/keycloakdb;NON_KEYWORDS=VALUE' --http-management-port 9001"
+# Copy the exported file from the container to the host
+docker cp $1:/tmp/elicit-realm-export.json ./vaadin-realm-export.json
